7331c47ab5c1b7bc3e93e523076c21adf9887aed
shubham <shubhamsatna9074@gmail.com>
2024-03-11 07:46:59 +0530

    First Commit;

---
+++ src/main/java/com/shubham/repo/UserInfoRepository.java
@@ -0,0 +1,13 @@
+package com.shubham.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shubham.entity.UserInfo;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> { 
+    Optional<UserInfo> findByName(String username); 
+}
